2c037f674b380072aacb12bdcce742c0ef1e038c
Jesse Millar <hellojessemillar@gmail.com>
2016-04-04 10:29:06 -0600

    Working example with test content

--- controllers/controllerGroup.go
+++
@@ -1,8 +0,0 @@
-package controllers
-
-import "github.com/jessemillar/web-chat/accessors"
-
-// ControllerGroup holds all config information for the controllers
-type ControllerGroup struct {
-	Accessors *accessors.AccessorGroup
-}
--- server.go
+++ server.go
@@ -9,17 +9,32 @@ import (
 	"github.com/zenazn/goji/web"
 )
 
+type chatMessage struct { // A message structure to put in the chat buffer
+	User    string
+	Message string
+}
+
+var chatBuffer []chatMessage // The chat buffer
+
 func health(c web.C, w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "%s\n", "Uh, we had a slight weapons malfunction, but uh... everything's perfectly all right now. We're fine. We're all fine here now, thank you. How are you?")
 }
 
-func postChat(c web.C, w http.ResponseWriter, r *http.Request) {
-	println("Stuff")
+func postMessage(c web.C, w http.ResponseWriter, r *http.Request) {
+	// Spec-defined query parameters
+	name := r.URL.Query().Get("name")
+	line := r.URL.Query().Get("line")
+
+	message := chatMessage{User: name, Message: line}
+	chatBuffer = append(chatBuffer, message)
+
+	fmt.Fprintf(w, "%s\n", chatBuffer)
 }
 
 func main() {
-	goji.Get("/health", health) // Service health
-	goji.Post("/chat ?name=<name>&line=<chat line>", postChat)
-	flag.Set("bind", ":9200")
-	goji.Serve()
+	goji.Get("/health", health)    // Service health
+	goji.Get("/chat", postMessage) // This hurts me because this is definitely not how a RESTful endpoint should work but the project spec demanded it
+	goji.Handle("/*", http.FileServer(http.Dir("content")))
+	flag.Set("bind", ":9020") // Set the port that Goji listens on
+	goji.Serve()              // Start listening
 }
