e17a59e272db73506192e6314e4efaadcb70c6db
Jesse Millar <hellojessemillar@gmail.com>
2016-04-05 08:02:38 -0600

    Should get data from the server

--- scripts/main.js
+++ scripts/main.js
@@ -1,19 +1,19 @@
-$(document).ready(function () {
-  populateChat();
+$(document).ready(function() {
+    populateChat();
 });
 
-function populateChat () {
-  $.ajax({
-    url: 'files\\data.json',
-    datatype: 'application/json',
-    success: function (data) {
-      var i = 0;
-      $("#chatbox").html("");
-      for(var i in data) { //Insert chat log into the #chatbox div
-      	$("#chatbox").append(data[i].User + " says: " + data[i].Message + "<br>"); 
-      }
-    }
-  });
+function populateChat() {
+    $.ajax({
+        url: 'http://woodsman.jessemillar.com:9020/chat',
+        datatype: 'application/json',
+        success: function(data) {
+            var i = 0;
+            $("#chatbox").html("");
+            for (var i in data) { //Insert chat log into the #chatbox div
+                $("#chatbox").append(data[i].User + " says: " + data[i].Message + "<br>");
+            }
+        }
+    });
 }
 
-var interval = setInterval(populateChat,1000);
+var interval = setInterval(populateChat, 1000);
