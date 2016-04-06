9452a67b5dba7564ed5ba7e4b7cd2336ae9bcf8c
Jesse Millar <hellojessemillar@gmail.com>
2016-04-05 19:14:47 -0600

    Trying a different method of triggering the AJAX call

--- scripts/main.js
+++ scripts/main.js
@@ -1,5 +1,5 @@
 $(document).ready(function() {
-    populateChat();
+    populateChat(); // Load chat messages on page start
 });
 
 function populateChat() {
@@ -8,8 +8,8 @@ function populateChat() {
         url: 'http://woodsman.jessemillar.com:9020/chat',
         datatype: 'application/json',
         success: function(data) {
-            data = JSON.parse(data);
-            $("#chatbox").html("");
+            data = JSON.parse(data); // Parse the JSON into an object
+            $("#chatbox").html(""); // Wipe the current messages
             for (var message in data) { // Insert chat log into the #chatbox div
                 $("#chatbox").append("<b>" + data[message].User + "</b>: " + data[message].Message + "<br>");
             }
@@ -17,15 +17,17 @@ function populateChat() {
     });
 }
 
-$('#send_chat').submit(function(e){
-    e.preventDefault();
+function sendMessage() {
+    console.log("Sending message");
     $.ajax({
-        url:'http://woodsman.jessemillar.com:9020/chat',
-        type:'POST',
-        success:function(){
-            $('#usrmsg').val(''); // Wipe the input field
+        type: "POST",
+        url: "http://woodsman.jessemillar.com:9020/chat?name=" + $('#name').val() + "&line=" + $('#line').val(),
+        success: function() {
+            console.log("Message sent");
+            $('#line').val(''); // Wipe the input field
+            populateChat(); // Get the chat log now for instant feedback
         }
     });
-});
+}
 
 var interval = setInterval(populateChat, 1000); // Refresh every second
