98860a32cbcecc00e7bee764a64b958730103d35
Jesse Millar <hellojessemillar@gmail.com>
2016-04-06 08:05:59 -0600

    Almost working

--- scripts/main.js
+++ scripts/main.js
@@ -1,13 +1,14 @@
-var username; // The user's username
+var username; // The user"s username
+var interval = setInterval(populateChat, 1000); // Refresh every second
 
 $(document).ready(function() {
     askName();
-    // populateChat(); // Load chat messages on page start
+    populateChat(); // Load chat messages on page start
 });
 
 function askName() {
     swal({
-        title: "What's your name?",
+        title: "What is your name?",
         text: "This will be your username.",
         type: "input",
         showCancelButton: false,
@@ -24,112 +25,96 @@ function askName() {
         }
 
         username = inputValue;
-
-        swal("Nice!", "Your username will be: " + inputValue + ".", "success");
+        $(".message_input").focus();
+        swal.close();
     });
 }
 
-// function populateChat() {
-//     console.log("Populating chat");
-//     $.ajax({
-//         url: 'http://woodsman.jessemillar.com:9020/chat',
-//         datatype: 'application/json',
-//         success: function(data) {
-//             data = JSON.parse(data); // Parse the JSON into an object
-//             $("#chatbox").html(""); // Wipe the current messages
-//             for (var message in data) { // Insert chat log into the #chatbox div
-//                 $("#chatbox").append("<b>" + data[message].User + "</b>: " + data[message].Message + "<br>");
-//             }
-//         }
-//     });
-// }
-//
-// function sendMessage() {
-//     console.log("Sending message");
-//     $.ajax({
-//         type: "POST",
-//         url: "http://woodsman.jessemillar.com:9020/chat?name=" + $('#name').val() + "&line=" + $('#line').val(),
-//         success: function() {
-//             console.log("Message sent");
-//             $('#line').val(''); // Wipe the input field
-//             populateChat(); // Get the chat log now for instant feedback
-//         }
-//     });
-// }
-//
-// var interval = setInterval(populateChat, 1000); // Refresh every second
+function populateChat() {
+    console.log("Populating chat");
+    $.ajax({
+        url: "http://woodsman.jessemillar.com:9020/chat",
+        datatype: "application/json",
+        success: function(data) {
+            data = JSON.parse(data); // Parse the JSON into an object
+            $(".messages").html(""); // Wipe the current messages
 
-(function() {
-    var Message = function(arg) {
-        this.text = arg.text;
-        this.message_side = arg.message_side;
-
-        this.draw = function(_this) {
-            return function() {
-                var $message = $($('.message_template').clone().html());
-                $message.addClass(_this.message_side).find('.text').html(_this.text);
-
-                $('.messages').append($message);
-
-                return setTimeout(function() {
-                    return $message.addClass('appeared');
-                }, 0);
-            };
-        }(this);
-
-        return this;
-    };
-
-    $(function() {
-        var getMessageText = function() {
-            var $message_input;
-            $message_input = $('.message_input');
-            return $message_input.val();
-        };
-
-        var clearInput = function() {
-            $('.message_input').val('');
-        };
-
-        var sendMessage = function(user, text, message_side) {
-            if (text.trim() === '') {
-                return;
+            for (var message in data) { // Insert chat log into the #chatbox div
+                postMessage(data[message].User, data[message].Message, "left");
             }
 
-            var $messages = $('.messages');
-
-            var message = new Message({
-                text: "<b>" + user + ":</b> " + text,
-                message_side: message_side
-            });
-
-            message.draw();
-
-            return $messages.animate({
-                scrollTop: $messages.prop('scrollHeight')
-            }, 300);
-        };
-
-        $('.send_message').click(function(e) { // On send button click
-            sendMessage(getMessageText(), "right");
-            clearInput();
-            return;
-        });
-
-        $('.message_input').keyup(function(e) {
-            if (e.which === 13) { // Enter key
-                sendMessage(getMessageText(), "right");
-                clearInput();
-                return;
-            }
-        });
-
-        sendMessage("Sandy", 'Hello Philip! :)', "left");
-        setTimeout(function() {
-            return sendMessage("Philip", 'Hi Sandy! How are you?', "right");
-        }, 1000);
-        return setTimeout(function() {
-            return sendMessage("Sandy", 'I\'m fine, thank you!', "left");
-        }, 2000);
+            $(".messages").animate({ // Scroll to the bottom of the chat window
+                scrollTop: $(".messages").prop("scrollHeight")
+            }, 10);
+        }
     });
-}.call(this));
+}
+
+var Message = function(arg) {
+    this.text = arg.text;
+    this.message_side = arg.message_side;
+
+    this.draw = function(_this) {
+        return function() {
+            var $message = $($(".message_template").clone().html());
+            $message.addClass(_this.message_side).find(".text").html(_this.text);
+
+            $(".messages").append($message);
+
+            return setTimeout(function() {
+                return $message.addClass("appeared");
+            }, 0);
+        };
+    }(this);
+
+    return this;
+};
+
+function clearInput() {
+    $(".message_input").val("");
+}
+
+function sendMessage() {
+    $.ajax({
+        type: "POST",
+        url: "http://woodsman.jessemillar.com:9020/chat?name=" + username + "&line=" + $(".message_input").val(),
+        success: function() {
+            console.log("Message sent");
+            $("#line").val(""); // Wipe the input field
+            populateChat(); // Get the chat log now for instant feedback
+        }
+    });
+}
+
+function postMessage(user, text, message_side) {
+    if (text.trim() === "") {
+        return;
+    }
+
+    var $messages = $(".messages");
+
+    var message = new Message({
+        text: "<b>" + user + ":</b> " + text,
+        message_side: message_side
+    });
+
+    message.draw();
+
+    return $messages.animate({
+        scrollTop: $messages.prop("scrollHeight")
+    }, 300);
+}
+
+$(".send_message").click(function(e) { // On "send" button click
+    sendMessage(username, $(".message_input").val(), "right");
+    clearInput();
+    return;
+});
+
+$(".message_input").keyup(function(e) {
+    if (e.which === 13) { // Enter key
+        sendMessage(username, $(".message_input").val(), "right");
+        clearInput();
+        return;
+    }
+});
